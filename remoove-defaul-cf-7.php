e14b7108022a68f567966432c6492442f7d6e3ae
Егор <35236837+Far-East@users.noreply.github.com>
2019-02-06 12:20:46 +0300

    Отключаем дефолтные стили Contact Form 7

---
+++ remoove-defaul-cf-7.php
@@ -0,0 +1,6 @@
+<?php
+// Отключаем дефолтные стили Contact Form 7
+function remove_default_stylesheet() {
+	wp_deregister_style( 'contact-form-7' );
+}
+add_action( 'wp_enqueue_scripts', 'remove_default_stylesheet', 20 ); 
